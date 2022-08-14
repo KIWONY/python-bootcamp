12e3f8c2dfae6eb13062eab4f30ca05ce908b508
KIWONY <kien1004@naver.com>
2022-08-14 16:34:40 +0900

    modify: Higher-Lower 게임(강의듣고 수정)

--- basics/higher_and_lower.py
+++ basics/higher_and_lower.py
@@ -1,46 +1,64 @@
-from game_data import data
-from random import randrange
+from higher_lower_game_data import data
+import random
+import os
 
-#랜덤으로 data에서 뽑아내기 
-def pick_data(n):
-    name = data[n]['name']
-    description = data[n]['description']
-    country = data[n]['country']
+def clear():
+    os.system('cls')
+
+def pick_data(dic):
+    # data 딕셔너리에서 3가지 항목 추출
+    name = dic['name']
+    description = dic['description']
+    country = dic['country']
     return f'{name} , {description}, from {country}'
+
+def check_answer(guess, follower_a, follower_b):
+    # 답 체크 
+    if follower_a > follower_b:
+        return guess == 'A'     # guess 가 A라면 True반환, B라면 False반환/ 어차피 같은 경우는 없음.
+    else:
+        return guess == 'B'
 
 score = 0 
 game_over = False
+data_b = random.choice(data)   
+ 
+while not game_over: 
+    print(f"You're current score is {score}")
+    # 리스트에서 직접 랜덤 추출
+    data_a = data_b
+    data_b = random.choice(data)
     
-while not game_over:
-    # 랜덤 숫자
-    index1 = randrange(len(data))
+    # 앞서 뽑은 data_b가 중복되지않게 하기 위해서 중복되지않을 때까지 반복
+    while data_a == data_b:
+        data_b = random.choice(data)
+
+    compare = pick_data(data_a)
+    against = pick_data(data_b)
+    print(f'Compare A : {compare}')
+    print(f'Against B : {against}')
     
-    follower1 = data[index1]['follower_count']
-    d1 = pick_data(index1)
-    print(f'Compare A : {d1}')
-    #agianst
-    index2 = randrange(len(data))
-    # 만약 같은 data를 뽑아냈다면 바꾸기
-    if index1 == index2:
-        index2 = randrange(len(data))
-    follower2 = data[index2]['follower_count']
-    d2 = pick_data(index2)
-    print(f'Against B : {d2}')
-    
-    answer = input('WHO has more followers? Type A or B: ')
-    if follower1 < follower2 and answer == 'B':
-        score += 1 
-        print(f"You're score is {score}")
-    elif follower1 > follower2 and answer == 'A':
+    guess = input('WHO has more followers? Type A or B: ').upper()
+    follower_a = data_a['follower_count']
+    follower_b = data_b['follower_count']
+    is_correct = check_answer(guess, follower_a, follower_b)
+
+    if is_correct == True:
         score += 1
         print(f"You're score is {score}")
-    else:
+        clear()
+    else: 
         print(f"WRONG! You're FINAL score is {score}")
         game_over = True
+
     
     
-
-
-
+    
+    
+    
+### 내가 부족했던 부분
+# 1. 리스트에서 랜덤으로 추출 (랜덤 숫자로 하면 번거로움)
+# 2. 팔로우 수 비교하는 함수 생성 
+# 3. b가 그다음 a가 되도록 
 
 
